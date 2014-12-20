dbde0a06a022cb88ccb5bf3c1a1b27e36b94cf06
OpenIntroOrg <openintro.org@gmail.com>
2014-12-20 16:04:43 -0600

    Minor updates to linear regression plotting functions

--- openintro/R/PlotWLine.R
+++ openintro/R/PlotWLine.R
@@ -7,23 +7,28 @@ PlotWLine <- function(x, y,
                       n = 4,
                       nMax = 4,
                       yR = 0.1,
+                      axes = TRUE,
                       ...){
   par(las = 0)
   yR <- range(y) + c(-1, 1) * 0.1 * diff(range(y))
   plot(x, y,
-       axes = FALSE,
        pch = 20,
        cex = cex,
        col = COL[1,2],
        xlab = '',
        ylab = '',
        ylim = yR,
+       axes = FALSE,
        ...)
-  buildAxis(1, x, n, nMax = nMax)
+  if (axes) {
+    buildAxis(1, x, n, nMax = nMax)
+  }
   mtext(xlab, 1, 1.8)
   mtext(ylab, 2, 2.8)
   par(mgp = c(2.8, 0.75, 0), las = 1)
-  buildAxis(2, y, n, nMax = nMax)
+  if (axes) {
+    buildAxis(2, y, n, nMax = nMax)
+  }
   g <- lm(y ~ x)
   abline(g, col = COL[5], lwd = 1.5)
 }
