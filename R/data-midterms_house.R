38f42654d49c7e086ec43baa70a3c3088867c204
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-04-11 05:11:04 -0400

    Docs for midterms house

---
+++ R/data-midterms_house.R
@@ -0,0 +1,21 @@
+#' President's party performance and unemployment rate
+#' 
+#' Covers midterm elections.
+#' 
+#' An older version of this data is at \code{\link{unemploy.pres}}.
+#' 
+#' @name midterms_house
+#' @docType data
+#' @format A data frame with 29 observations on the following 5 variables.
+#' \describe{ \item{list("year")}{a numeric vector} \item{list("potus")}{The
+#' president in office.} \item{list("party")}{President's party.}
+#' \item{list("unemp")}{Unemployment rate.} \item{list("house_change")}{ Change
+#' in House seats for the president's party.  } }
+#' @source Wikipedia.
+#' @keywords datasets
+#' @examples
+#' 
+#' data(midterms_house)
+#' plot(midterms_house$unemp, midterms_house$change)
+#' 
+"midterms_house"
