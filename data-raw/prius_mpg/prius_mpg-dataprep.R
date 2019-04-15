4a3be8890ff866d7493e22e0259d86ae6a055fdb
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-04-15 00:42:38 -0400

    Add prius user mileage data

---
+++ data-raw/prius_mpg/prius_mpg-dataprep.R
@@ -0,0 +1,13 @@
+# load packages ----------------------------------------------------------------
+library(tidyverse)
+library(lubridate)
+
+# load data --------------------------------------------------------------------
+prius_mpg_raw <- read_csv(here::here("data-raw/prius_mpg", "prius_mpg.csv"))
+
+# fix dates --------------------------------------------------------------------
+prius_mpg <- prius_mpg_raw %>%
+  mutate(last_updated = mdy(last_updated))
+
+# save -------------------------------------------------------------------------
+use_data(prius_mpg)
