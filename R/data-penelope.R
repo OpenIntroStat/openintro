3396ad8d9281b9547b0998d583bf8ea009c8ac88
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-04-20 00:37:50 -0400

    Make penelope a tibble and use Roxygen documentation

---
+++ R/data-penelope.R
@@ -0,0 +1,23 @@
+#' Guesses at the weight of Penelope (a cow)
+#'
+#' The data was collected by the Planet Money podcast to test a theory about
+#' crowd-sourcing. Penelope's actual weight was 1,355 pounds.
+#'
+#' @name penelope
+#' @docType data
+#' @format A tibble with 17,184 observations on the following variable.
+#' \describe{
+#'   \item{weight}{Guesses of Penelope's weight, in pounds.}
+#'}
+#' @source \url{https://www.npr.org/sections/money/2015/08/07/429720443/17-205-people-guessed-the-weight-of-a-cow-heres-how-they-did}
+#' @keywords datasets
+#' @examples
+#'
+#' library(ggplot2)
+#'
+#' ggplot(penelope, aes(x = weight)) +
+#'   geom_histogram(binwidth = 250)
+#'
+#' summary(penelope$weight)
+#'
+"penelope"
