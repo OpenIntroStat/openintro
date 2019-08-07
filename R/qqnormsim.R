f9803084a96882742314d2de78edc3ca602ff167
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2019-08-07 03:04:06 +0100

    Fix qqnormsim

--- R/qqnormsim.R
+++ R/qqnormsim.R
@@ -8,19 +8,20 @@
 #' @param sample the variable to be plotted.
 #' @param data data frame to use.
 #'
-#' @importFrom ggplot2 qplot
+#' @importFrom ggplot2 ggplot
+#' @importFrom stats rnorm sd
 #' @return A 3 x 3 grid of qqplots.
 #' @export
 qqnormsim <- function(sample, data) {
   y <- eval(substitute(sample), data)
-  simnorm <- rnorm(n = length(y) * 8, mean = mean(y),
-                   sd = sd(y))
+  simnorm <- stats::rnorm(n = length(y) * 8, mean = mean(y),
+                          sd = stats::sd(y))
   df <- data.frame(x       = c(y, simnorm),
                    plotnum = rep(c("data", "sim 1", "sim 2",
                                    "sim 3", "sim 4", "sim 5",
                                    "sim 6", "sim 7", "sim 8"),
                                  each = length(y)))
-  ggplot(data = df, aes_string(sample = "x")) +
-    stat_qq() +
-    facet_wrap( ~ plotnum)
+  ggplot2::ggplot(data = df, ggplot2::aes_string(sample = "x")) +
+    ggplot2::stat_qq() +
+    ggplot2::facet_wrap( ~ plotnum)
 }
