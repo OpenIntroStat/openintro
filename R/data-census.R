bb6cf17e475daafd0154ad5db2c6957782cc85b5
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-06-26 16:57:41 +0100

    Formatting

--- R/data-census.R
+++ R/data-census.R
@@ -12,13 +12,8 @@
 #'   \item{total_family_income}{Total family income (in U.S. dollars).}
 #'   \item{age}{Age.}
 #'   \item{sex}{Sex with levels \code{Female} and \code{Male}.}
-#'   \item{race_general}{Race with levels \code{American
-#' Indian or Alaska Native}, \code{Black}, \code{Chinese}, \code{Japanese},
-#' \code{Other Asian or Pacific Islander}, \code{Two major races}, \code{White}
-#' and \code{Other}.}
-#'   \item{marital_status}{Marital status with levels
-#' \code{Divorced}, \code{Married/spouse absent}, \code{Married/spouse
-#' present}, \code{Never married/single}, \code{Separated} and \code{Widowed}.}
+#'   \item{race_general}{Race with levels \code{American Indian or Alaska Native}, \code{Black}, \code{Chinese}, \code{Japanese}, \code{Other Asian or Pacific Islander}, \code{Two major races}, \code{White} and \code{Other}.}
+#'   \item{marital_status}{Marital status with levels \code{Divorced}, \code{Married/spouse absent}, \code{Married/spouse present}, \code{Never married/single}, \code{Separated} and \code{Widowed}.}
 #'   \item{total_personal_income}{Total personal income (in U.S. dollars).}
 #' }
 #' @source \url{http://factfinder.census.gov}
