f6372de1039e5075e87bd8e1e2ab194819a57894
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-09-08 12:47:14 +0100

    Add exam_grades data

---
+++ data-raw/exam_grades/exam_grades-dataprep.R
@@ -0,0 +1,18 @@
+# Load packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(usethis)
+library(here)
+
+# Read data --------------------------------------------------------------------
+
+exam_grades <- read_tsv(here::here("data-raw/exam_grades", "exam_grades.txt"))
+
+# Recode sex -------------------------------------------------------------------
+
+exam_grades <- exam_grades %>%
+  mutate(sex = if_else(sex == "M", "Man", "Woman"))
+
+# Add to package ---------------------------------------------------------------
+
+use_data(exam_grades, overwrite = TRUE)
