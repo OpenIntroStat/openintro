727ae864c062b6cf5f1c2583037a76fe7baf3748
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-01-03 13:29:26 +0000

    Clean up data prep

--- data-raw/ssd_speed/ssd_speed-dataprep.R
+++ data-raw/ssd_speed/ssd_speed-dataprep.R
@@ -1,20 +1,14 @@
-library(readr)
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
 library(usethis)
 library(here)
 library(janitor)
-library(dplyr)
 
 # load data --------------------------------------------------------------------
 
-ssd_speed <- read.csv(here::here("data-raw/ssd_speed/ssd_speed.csv"))
-
-# cleaning: clean variable names
-
-ssd_speed <- ssd_speed %>%
-  janitor::clean_names() %>%
-  rename(brand = i_brand)
-
+ssd_speed <- read_csv(here::here("data-raw/ssd_speed/ssd_speed.csv"))
 
 # save -------------------------------------------------------------------------
 
-usethis::use_data(ssd_speed, overwrite= TRUE)
+usethis::use_data(ssd_speed, overwrite = TRUE)
