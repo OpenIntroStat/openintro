d1f287ffbade7c1c5674f672ba240c41f2d5ab4a
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-01-03 13:14:47 +0000

    Clean up data prep

--- data-raw/salinity/salinity-dataprep.R
+++ data-raw/salinity/salinity-dataprep.R
@@ -1,18 +1,12 @@
-library(readr)
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
 library(usethis)
 library(here)
-library(dplyr)
-library(janitor)
 
 # load data --------------------------------------------------------------------
 
-salinity <- read.csv(here::here("data-raw/salinity/salinity.csv"))
-
-# cleaning: clean variable names
-
-salinity <- janitor::clean_names(salinity) %>%
-  rename(site_number = i_site_number)
-
+salinity <- read_csv(here::here("data-raw/salinity/salinity.csv"))
 
 # save -------------------------------------------------------------------------
 
