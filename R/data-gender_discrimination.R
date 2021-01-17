639ffaa4e0b88696d954c776d7886b9d359d73f2
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-01-17 09:25:37 +0000

    Align docs with opportunity_cost

--- R/data-gender_discrimination.R
+++ R/data-gender_discrimination.R
@@ -6,16 +6,20 @@
 #' @name gender_discrimination
 #' @docType data
 #' @format A data frame with 48 observations on the following 2 variables.
-#' \describe{\item{gender}{a factor with levels \code{female}
-#' \code{male}} \item{decision}{a factor with levels \code{not}
-#' \code{promoted}} }
+#' \describe{
+#'   \item{gender}{a factor with levels \code{female} and \code{male}}
+#'   \item{decision}{a factor with levels \code{not promoted} and \code{promoted}}
+#'   }
 #' @source Rosen B and Jerdee T. 1974. Influence of sex role stereotypes on
 #' personnel decisions. Journal of Applied Psychology 59(1):9-14.
 #' @keywords datasets
 #' @examples
 #'
-#' library(dplyr)
+#' library(ggplot2)
 #'
 #' table(gender_discrimination)
+#'
+#' ggplot(gender_discrimination, aes(y = gender, fill = decision)) +
+#'   geom_bar(position = "fill")
 #'
 "gender_discrimination"
