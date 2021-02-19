454fcafcf8a10103dcaab92aea2633bc8ede5845
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-02-19 12:07:22 +0000

    Does this help?

--- R/makeTube.R
+++ R/makeTube.R
@@ -51,26 +51,6 @@
 #' plot(gradestv)
 #' makeTube(gradestv$tv, gradestv$grades, 1.5, type='robust', stDev='o')
 #'
-#' # what can go wrong with a basic least squares model
-#' par(mfrow = c(1,3), mar = c(2.5, 2.5, 1, 2.5))
-#' # 1
-#' x <- runif(100)
-#' y <- 25*x-20*x^2+rnorm(length(x), sd=1.5)
-#' plot(x,y)
-#' makeTube(x,y,type='q')
-#' # 2
-#' x <- c(-0.6, -0.46, -0.091, runif(97))
-#' y <- 25*x + rnorm(length(x))
-#' y[2] <- y[2] + 8
-#' y[1] <- y[1] + 1
-#' plot(x,y,ylim=range(y)+c(-10,5))
-#' makeTube(x,y)
-#' # 3
-#' x <- runif(100)
-#' y <- 5*x + rnorm(length(x), sd=x)
-#' plot(x,y)
-#' makeTube(x, y, stDev='l', bw=0.03)
-#'
 makeTube <- function(x, y,
                      Z = 2,
                      R = 1,
