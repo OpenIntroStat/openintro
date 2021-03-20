d02c7c621d7ae79040dc56522ec75b2a2f2a2a38
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-03-20 15:37:24 +0000

    Fix midterms_house party levels

--- R/data-midterms_house.R
+++ R/data-midterms_house.R
@@ -8,11 +8,11 @@
 #' @docType data
 #' @format A data frame with 29 observations on the following 5 variables.
 #' \describe{
-#'   \item{year}{a numeric vector}
+#'   \item{year}{Year.}
 #'   \item{potus}{The president in office.}
-#'   \item{party}{President's party.}
+#'   \item{party}{President's party: Democrat or Republican.}
 #'   \item{unemp}{Unemployment rate.}
-#'   \item{house_change}{Change in House seats for the president's party.}
+#'   \item{house_change}{Change in House seats for the President's party.}
 #'   }
 #' @source Wikipedia.
 #' @keywords datasets
