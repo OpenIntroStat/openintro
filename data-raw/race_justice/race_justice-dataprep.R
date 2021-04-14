aa626b1aa910af281e0d443743e2a77b7a267137
David Diez <david.m.diez@gmail.com>
2021-04-14 15:15:11 -0700

    Fix typos for `race_justice` data set and documentation (#44)
    * Initalize COVID-19 positive testing rate and outcome data
    
    * Add COVID-19 data for checking importance of test positivity rating in predicting deaths
    
    * Fix typos for race_justice data set and documentation
    
    * Update race_justice.Rd (forgot to run  before)
    
    * Remove COVID data set not yet ready for distribution
    
    * Not including this dataset in this release
    
    * Make race_justice into df with rows as observations
    
    Co-authored-by: Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>

---
+++ data-raw/race_justice/race_justice-dataprep.R
@@ -0,0 +1,21 @@
+# Load packages
+library(tidyverse)
+library(usethis)
+
+# Load data
+race_justice_table <- read_csv(here::here("data-raw/race_justice/race-justice-table.csv"))
+
+race_justice <- race_justice_table %>%
+  select(-unweighted_n) %>%
+  pivot_longer(cols = -race, names_to = "response", values_to = "n") %>%
+  uncount(weights = n) %>%
+  rename(race_eth = race) %>%
+  mutate(
+    response = if_else(response == "not_sure", "not sure", response),
+    response = str_to_sentence(response),
+    response = fct_relevel(response, "Yes", "No", "Not sure"),
+    race_eth = fct_relevel(race_eth, "White", "Black", "Hispanic", "Other")
+    )
+
+# Add to package
+use_data(race_justice, overwrite = TRUE)
