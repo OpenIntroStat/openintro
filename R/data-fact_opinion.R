e165ad291e3dae63617341eb48eb6eb8fb86c41f
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-04-14 23:42:01 +0100

    Clean up fact_opinion

--- R/data-fact_opinion.R
+++ R/data-fact_opinion.R
@@ -1,21 +1,67 @@
 #' Can Americans categorize facts and opinions?
 #'
-#' Pew Research Center conducted a survey in 2018, asking
-#' participants to categorize five factual and five opinion statements.
+#' Pew Research Center conducted a survey in 2018, asking a sample of
+#' U.S. adults to categorize five factual and five opinion statements.
+#' This dataset provides data from this survey, with information on the age
+#' group of the participant as well as the number of factual and opinion
+#' statements they classified correctly (out of 5).
 #'
-#'
-#' @format A data frame with 5035 rows and 3 variables.
+#' @format A data frame with 5,035 rows and 3 variables.
 #' \describe{
 #'   \item{age_group}{Age group of survey participant.}
 #'   \item{fact_correct}{Number of factual statements classified correctly (out of 5).}
 #'   \item{opinion_correct}{Number of opinion statements classified correctly (out of 5).}
 #' }
 #' @examples
-#' boxplot(fact_correct~age_group, data = fact_opinion,
-#'   main = "Number of Factual Statements Correctly Categorized",
-#'   xlab = "Age Group", ylab = "Number Correct (out of 5)")
 #'
-#' @source
-#'    \href{https://www.pewresearch.org/fact-tank/2018/10/23/younger-americans-are-better-than-older-americans-at-telling-factual-news-statements-from-opinions/}{Pew Research Center}
+#' library(ggplot2)
+#'
+#' # Distribution of fact_correct by age group
+#' ggplot(fact_opinion, aes(x = age_group, y = fact_correct)) +
+#'   geom_boxplot() +
+#'   labs(
+#'     x = "Age group",
+#'     y = "Number correct (factual)",
+#'     title = "Number of factual statements classified correctly by age group"
+#'     )
+#'
+#' # Distribution of opinion_correct by age group
+#' ggplot(fact_opinion, aes(x = age_group, y = opinion_correct)) +
+#'   geom_boxplot() +
+#'   labs(
+#'     x = "Age group",
+#'     y = "Number correct (opinion)",
+#'     title = "Number of opinion statements classified correctly by age group"
+#'     )
+#'
+#' # Replicating the figure from Pew report (see source for link)
+#' fact_opinion %>%
+#'   mutate(
+#'     facts = case_when(
+#'       fact_correct <= 2        ~ "Two or fewer",
+#'       fact_correct %in% c(3,4) ~ "Three or four",
+#'       fact_correct == 5        ~ "All five"
+#'       ),
+#'     facts = fct_relevel(facts, "Two or fewer", "Three or four", "All five"),
+#'     opinions = case_when(
+#'       opinion_correct <= 2        ~ "Two or fewer",
+#'       opinion_correct %in% c(3,4) ~ "Three or four",
+#'       opinion_correct == 5        ~ "All five"
+#'       ),
+#'     opinions = fct_relevel(opinions, "Two or fewer", "Three or four", "All five")
+#'   ) %>%
+#'   select(-fact_correct, -opinion_correct) %>%
+#'   pivot_longer(cols = -age_group, names_to = "question_type", values_to = "n_correct") %>%
+#'   ggplot(aes(y = fct_rev(age_group), fill = n_correct)) +
+#'   geom_bar(position = "fill") +
+#'   facet_wrap(~question_type, ncol = 1) +
+#'   scale_fill_viridis_d(guide = guide_legend(reverse = TRUE)) +
+#'   labs(
+#'     x = "Proportion",
+#'     y = "Age group",
+#'     fill = "Number of\ncorrect\nclassifications"
+#'   )
+#'
+#' @source [Younger Americans are better than older Americans at telling factual news statements from opinions](https://www.pewresearch.org/fact-tank/2018/10/23/younger-americans-are-better-than-older-americans-at-telling-factual-news-statements-from-opinions/), Pew Research Center, October 23, 2018.
 #'
 "fact_opinion"
--- data-raw/fact_opinion/fact_opinion-dataprep.R
+++ data-raw/fact_opinion/fact_opinion-dataprep.R
@@ -1,13 +1,15 @@
+# load packages ----------------------------------------------------------------
+
 library(tidyverse)
 library(foreign)
 
 # load data --------------------------------------------------------------------------
 
-raw_data <- read.spss("f_v_o_short_data.sav", to.data.frame = TRUE)
+fact_opinion_raw <- read.spss(here::here("data-raw/fact_opinion/f_v_o_short_data.sav"), to.data.frame = TRUE)
 
 # cleaning: select data related to Q8 of survey and format to match source publication
 
-fact_opinion <- raw_data %>%
+fact_opinion <- fact_opinion_raw %>%
   select(c("PPAGE","fact_fact","opinion_opinion")) %>%
   rename(age = PPAGE,
          fact_correct = fact_fact,
@@ -15,7 +17,9 @@ fact_opinion <- raw_data %>%
   mutate(age = as.numeric(as.character(age))) %>%
   mutate(age_group = case_when(age < 50 ~ "18-49",
                                TRUE ~ "50+")) %>%
-  select(-age)
+  select(-age) %>%
+  relocate(age_group) %>%
+  as_tibble()
 
 # Save -------------------------------------------------------------------------------
 
