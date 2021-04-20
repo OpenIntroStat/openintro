d530cf65f16a7ea33a05d2eced3f049ad468e8e3
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-04-20 10:19:52 +0100

    Add new dataset: lizards

---
+++ R/data-lizards.R
@@ -0,0 +1,36 @@
+#' Lizard speeds
+#'
+#' Data on top speeds measured on a laboratory race track for two species
+#' of lizards: Western fence lizard (Sceloporus occidentalis) and
+#' Sagebrush lizard (Sceloporus graciosus).
+#'
+#' @name lizards
+#' @docType data
+#' @format A data frame with 48 observations on the following 3 variables.
+#' \describe{
+#'   \item{top_speed}{Top speed of lizard, meters per second.}
+#'   \item{common_name}{Common name: `Western fence lizard` and `Sagebrush lizard`.}
+#'   \item{scientific_name}{Scientific name (Genus and species): `Sceloporus occidentalis` and `Sceloporus graciosus`.}
+#' }
+#' @source Adolph, S. C. 1987.  Physiological and behavioral ecology of the lizards
+#' Sceloporus occidentalis and Sceloporus graciosus. Dissertation.
+#' University of Washington, Seattle, Washington, USA.
+#' @keywords datasets
+#' @examples
+#' library(ggplot2)
+#' library(dplyr)
+#'
+#' # Top speed by species
+#' ggplot(lizards, aes(x = top_speed, color = common_name, fill = common_name)) +
+#'   geom_density(alpha = 0.5)
+#'
+#' # Top speed summary statistics by species
+#' lizards %>%
+#'   group_by(common_name) %>%
+#'   summarise(
+#'     n    = n(),
+#'     mean = mean(top_speed),
+#'     sd   = sd(top_speed)
+#'   )
+#'
+"lizards"
