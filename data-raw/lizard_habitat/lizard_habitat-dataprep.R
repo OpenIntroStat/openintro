ddd0b38f9fc78401fb4c7df223772dd768594bb7
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-04-22 13:03:51 +0100

    Update lizard datasets

---
+++ data-raw/lizard_habitat/lizard_habitat-dataprep.R
@@ -0,0 +1,21 @@
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(readxl)
+library(usethis)
+
+# load data -------------------------------------------------------------------
+
+lizard_habitat <- read_excel(here::here("data-raw/lizard_habitat/S. occidentalis perch sunlight data for Jo Hardin.xls"),
+                             skip = 14)
+
+lizard_habitat %>%
+  mutate(
+    site = as.factor(site),
+    sunlight = as.factor(sunlight),
+    sunlight = fct_relevel(sunlight, "sun", "partial", "share")
+  )
+
+# Save ------------------------------------------------------------------------
+
+usethis::use_data(lizard_habitat, overwrite = TRUE)
