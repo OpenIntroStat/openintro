520ee0f5f8ca21446bce8684f6f5db682be08ee4
Nick Paterno <48722864+npaterno@users.noreply.github.com>
2021-07-29 08:16:42 -0700

    Update R/data-mlb_teams.R
    committing suggested change
    
    Co-authored-by: Mine Cetinkaya-Rundel <cetinkaya.mine@gmail.com>

--- R/data-mlb_teams.R
+++ R/data-mlb_teams.R
@@ -57,10 +57,10 @@
 #'   select(year,team_name,ball_park)
 #'
 #' # List the teams with their average number of wins and losses
-#'  mlb_teams %>%
-#'    group_by(team_name)%>%
-#'    summarize(mean_wins=mean(wins),mean_losses=mean(losses))%>%
-#'    arrange((team_name))
+#' mlb_teams %>%
+#'   group_by(team_name)%>%
+#'   summarize(mean_wins=mean(wins),mean_losses=mean(losses))%>%
+#'   arrange((team_name))
 #'
 #' @source [Lahmans Baseball Database](http://www.seanlahman.com/baseball-archive/statistics/)
 #'
