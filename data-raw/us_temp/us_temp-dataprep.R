ea7fc30e22c42328e200e7b163874ce05f35ee9a
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2023-09-25 23:53:42 -0400

    Fix data prep issues and add pkgs ex depends on

--- data-raw/us_temp/us_temp-dataprep.R
+++ data-raw/us_temp/us_temp-dataprep.R
@@ -1,34 +1,33 @@
 # Load packages ----------------------------------------------------------------
 
-library(openintro)
 library(tidyverse)
-library(usethis)
-library(lubridate)
 
 # Load data --------------------------------------------------------------------
 
 temp1950_raw <- read_csv(here::here("data-raw/us_temp/us_temp_1950.csv"))
 temp2022_raw <- read_csv(here::here("data-raw/us_temp/us_temp_2022.csv"))
 
-
-# Cleaning --------------------------------------------------------------------
+# prep data --------------------------------------------------------------------
 
 us_temp_1950 <- temp1950_raw |>
   janitor::clean_names() |>
   group_by(station) |>
   mutate(tmax_na = sum(is.na(tmax))) |>
-  filter(tmax_na < 5)
+  filter(tmax_na < 5) |>
+  ungroup()
 
-stations_1950 <- table(us_temp_1950$station) |> names()
+stations_1950 <- table(us_temp_1950$station) |>
+  names()
 
-
-us_temp_2022 <- temp1950_raw |>
+us_temp_2022 <- temp2022_raw |>
   janitor::clean_names() |>
   group_by(station) |>
   mutate(tmax_na = sum(is.na(tmax))) |>
-  filter(tmax_na < 5)
+  filter(tmax_na < 5) |>
+  ungroup()
 
-stations_2022 <- table(us_temp_2022$station) |> names()
+stations_2022 <- table(us_temp_2022$station) |>
+  names()
 
 station_list <- intersect(stations_1950, stations_2022)
 
@@ -38,5 +37,6 @@ us_temp <- us_temp_1950 |>
   select(-tavg, -tobs, -tmax_na) |>
   mutate(year = as.factor(year(date)))
 
-# Add to package
-use_data(us_temp, overwrite = TRUE)
+# save data –-------------------------------------------------------------------
+
+usethis::use_data(us_temp, overwrite = TRUE)
