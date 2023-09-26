8caade5dd74eaabdc4c4b3e8b06f5c304c62b97b
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2023-09-25 21:11:57 -0400

    Update paralympic data prep and example

--- R/data-paralympic_1500.R
+++ R/data-paralympic_1500.R
@@ -1,7 +1,7 @@
 #' Race time for Olympic and Paralympic 1500m.
 #'
 #' Compiled gold medal times for the 1500m race in the Olympic Games and the
-#' Paralympic Games.  The times given for contestants competing in
+#' Paralympic Games. The times given for contestants competing in
 #' the Paralympic Games are for athletes with different visual impairments;
 #' T11 indicates fully blind (with an option to race with a guide-runner)
 #' with T12 and T13 as lower levels of visual impairment.
@@ -23,25 +23,30 @@
 #' @source [https://www.paralympic.org/](https://www.paralympic.org/) and [https://en.wikipedia.org/wiki/1500_metres_at_the_Olympics](https://en.wikipedia.org/wiki/1500_metres_at_the_Olympics).
 #' @keywords datasets
 #' @examples
+#'
 #' library(ggplot2)
 #' library(dplyr)
 #'
-#' plot_data <- paralympic_1500 |>
-#'     mutate(Sight_Level = case_when(
-#'         type == "T11" ~ "total impairment",
-#'         type == "T12" ~ "some impairment",
-#'         type == "T13" ~ "some impairment",
-#'         type == "Olympic" ~ "no impairment")) |>
+#' paralympic_1500 |>
+#'   mutate(
+#'     sight_level = case_when(
+#'       type == "T11" ~ "total impairment",
+#'       type == "T12" ~ "some impairment",
+#'       type == "T13" ~ "some impairment",
+#'       type == "Olympic" ~ "no impairment"
+#'     )
+#'   ) |>
 #'   filter(division == "Men", year > 1920) |>
-#'   filter(type == "Olympic" | type == "T11")
-#'
-#' ggplot(plot_data, aes(x = year, y = time, color = Viz_Level)) +
+#'   filter(type == "Olympic" | type == "T11") |>
+#'   ggplot(aes(x = year, y = time_min, color = sight_level, shape = sight_level)) +
 #'   geom_point() +
 #'   scale_x_continuous(breaks = seq(1924, 2020, by = 8)) +
 #'   labs(
-#'     title = "Men's Olympic and Paralympic 1500m",
+#'     title = "Men's Olympic and Paralympic 1500m race times",
 #'     x = "Year",
-#'     y = "Time of Race"
+#'     y = "Time of Race (minutes)",
+#'     color = "Sight level",
+#'     shape = "Sight level"
 #'   )
 #'
 "paralympic_1500"
