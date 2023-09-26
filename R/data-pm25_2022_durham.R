b1a3a9a539f13482c8ada9768141c3fa784a3de2
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2023-09-25 22:22:11 -0400

    Update 2022 durham pm25 data + align 2011 data

--- R/data-pm25_2022_durham.R
+++ R/data-pm25_2022_durham.R
@@ -9,30 +9,32 @@
 #' @format A data frame with 356 observations on the following 20 variables.
 #' \describe{
 #'   \item{date}{Date.}
-#'   \item{source}{Source.}
-#'   \item{site_id}{The numeric site ID.}
+#'   \item{aqs_site_id}{The numeric site ID.}
 #'   \item{poc}{A numeric vector, the Parameter Occurance Code.}
 #'   \item{daily_mean_pm2_5_concentration}{A numeric vector with the average daily concentration of fine particulates, or particulate matter 2.5.}
-#'   \item{units}{A factor with levels \code{ug/m3 LC}.}
+#'   \item{units}{A character vector with value \code{ug/m3 LC}.}
 #'   \item{daily_aqi_value}{A numeric vector with the daily air quality index.}
-#'   \item{site_name}{A factor value with level \code{Durham Armory}.}
 #'   \item{daily_obs_count}{A numeric vector.}
 #'   \item{percent_complete}{A numeric vector.}
 #'   \item{aqs_parameter_code}{A numeric vector.}
-#'   \item{aqs_parameter_desc}{A factor with level \code{PM2.5 - Local Conditions}.}
+#'   \item{aqs_parameter_desc}{A factor vector with level \code{PM2.5 - Local Conditions}.}
 #'   \item{cbsa_code}{A numeric vector.}
-#'   \item{cbsa_name}{A factor with level \code{Durham-Chapel Hill, NC}.}
+#'   \item{cbsa_name}{A character vector with value \code{Durham-Chapel Hill, NC}.}
 #'   \item{state_code}{A numeric vector.}
-#'   \item{state}{A factor with level \code{North Carolina}.}
+#'   \item{state}{A character vector with value \code{North Carolina}.}
 #'   \item{county_code}{A numeric vector.}
-#'   \item{county}{A factor with level \code{Durham}.}
+#'   \item{county}{A character vector with value \code{Durham}.}
 #'   \item{site_latitude}{A numeric vector of the latitude.}
 #'   \item{site_longitude}{A numeric vector of the longitude.}
+#'   \item{site_name}{A character vector with value \code{Durham Armory}.}
 #'   }
-#' @source US Environmental Protection Agency, AirData, 2011.
+#' @source US Environmental Protection Agency, AirData, 2022.
 #' \url{http://www3.epa.gov/airdata/ad_data_daily.html}
 #' @keywords datasets
 #' @examples
 #'
-#' pm25_2011_durham
-"pm25_2011_durham"
+#' library(ggplot2)
+#' 
+#' ggplot(pm25_2022_durham, aes(x = date,  y = daily_mean_pm2_5_concentration, group = 1)) +
+#'   geom_line()
+"pm25_2022_durham"
--- data-raw/pm25_2022_durham/pm25_2022_durham-dataprep.R
+++ data-raw/pm25_2022_durham/pm25_2022_durham-dataprep.R
@@ -1,16 +1,30 @@
-library(readr)
+# load packages ----------------------------------------------------------------
+
+library(tidyverse)
 library(janitor)
 
-
 # load data ---------------------------------------------------------------------
-raw_data <- read.csv(here::here("data-raw/pm25_2022_durham/pm25_2022_durham.csv"),
-                     stringsAsFactors = TRUE)
 
-# cleaning: none required
+pm25_2022_durham_raw <- read_csv(here::here("data-raw/pm25_2022_durham/pm25_2022_durham.csv"))
 
-pm25_2022_durham <- raw_data |>
-  janitor::clean_names()
+# cleaning to match 2011 data ---------------------------------------------------
 
-# save --------------------------------------------------------------------------
+pm25_2022_durham <- pm25_2022_durham_raw |>
+  clean_names() |>
+  mutate(date = mdy(date)) |>
+  select(-source) |>
+  rename(aqs_site_id = site_id) |>
+  mutate(aqs_site_id = as.character(aqs_site_id)) |>
+  mutate(poc = as.integer(poc)) |>
+  mutate(daily_obs_count = as.integer(daily_obs_count)) |>
+  mutate(percent_complete = as.integer(percent_complete)) |>
+  mutate(aqs_parameter_code = as.integer(aqs_parameter_code)) |>
+  mutate(cbsa_code = as.integer(cbsa_code)) |>
+  mutate(state_code = as.integer(state_code)) |>
+  mutate(county_code = as.integer(county_code)) |>
+  relocate(site_name, .after = site_longitude) |>
+  mutate(aqs_parameter_desc = as.factor(aqs_parameter_desc))
+
+# save data --------------------------------------------------------------------
 
 usethis::use_data(pm25_2022_durham, overwrite = TRUE)
