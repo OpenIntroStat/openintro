89c59467db26cde6431f4392e9f0cfb4c0be9575
hardin47 <jo.hardin@pomona.edu>
2023-11-02 14:18:58 -0700

    forgot to save before committing

--- data-raw/us_temperature/us_temperature-dataprep.R
+++ data-raw/us_temperature/us_temperature-dataprep.R
@@ -7,7 +7,7 @@ library(lubridate)
 
 # Load data --------------------------------------------------------------------
 
-us_temperature <- read_csv("data-raw/us_temp/us_temperature.csv") |>
+us_temperature <- read_csv("data-raw/us_temperature/us_temperature.csv") |>
   select(location, everything())
 
 # Add to package
