aceb5d036c2dd6f847bafb1f231a6ac253ad5d02
npaterno <nicholas.paterno@gmail.com>
2026-08-13 09:48:46 -0700

    climate action data

---
+++ R/data-generation_climate_action.R
@@ -0,0 +1,18 @@
+#' Fake data for a gear company example
+#'
+#' A Pew Research poll published in May of 2021 looks at how Americans’
+#' attitudes about climate change differ by generation, party and other factors.
+#'
+#'
+#' @name generation_climate_data
+#' @docType data
+#' @format A data frame with 13664 observations on the following 2 variables.
+#' \describe{
+#'    \item{\code{genaration}}{a factor with levels \code{Gen Z, Millenial, Gen X} and \code{Boomer & older}}
+#'    \item{\code{response}}{a factor with levels \code{Took action} and \code{Didn't take action}}
+#'  }
+#' @keywords datasets
+#'
+#' @source Pew Research Center, American Trends Panel. Survey conducted April 20 - 29, 2021. "Gen Z, Millennials Stand Out for Climate Change Activism, Social Media Engagement With Issue"
+#'
+"generation_climate_action"
